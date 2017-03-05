74026e3071bcffde5afae2c13037db948fc99f1d
Duncan Evans <duncan.gb.evans@gmail.com>
2017-03-05 13:03:04 -0800

    Add project files.

---
+++ TheDinnerQuestion/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TheDinnerQuestion.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ TheDinnerQuestion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheDinnerQuestion.Global" Language="C#" %>
